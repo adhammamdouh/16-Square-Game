9dbf0922a61861c341edb2be64ba47c479d8df7d
adhammamdouh <36306083+adhammamdouh@users.noreply.github.com>
2019-03-15 13:54:03 +0200

    Update ClientThread.java

--- ClientThread.java
+++ ClientThread.java
@@ -2,32 +2,43 @@ import java.net.Socket;
 import java.io.*;
 
 public class ClientThread extends Thread {
-    protected Socket socket;
-    protected DataInputStream Read;
-    protected DataOutputStream Write;
+    protected Socket socket = null;
+    protected DataInputStream Receive = null;
+    protected DataOutputStream Send = null;
 
     public ClientThread(Socket clientSocket) throws IOException {
         this.socket = clientSocket;
-        Read = new DataInputStream(socket.getInputStream());
-        Write = new DataOutputStream(socket.getOutputStream());
-        System.out.println("Client accepted");
+        Receive = new DataInputStream(socket.getInputStream());
+        Send = new DataOutputStream(socket.getOutputStream());
+        System.out.println("One client Connected to server : " + socket.getPort());
     }
 
     public void run() {
         String Line = "";
         while (true) {
             try {
-                Line = Read.readUTF();
-                if(Line.equals("out")){
-                    socket.close();
+                Line = Receive.readUTF();
+                if(Line.equals("Exit")) {
+                    Send.writeUTF("Connection closed");
                     break;
                 }
-                Write.writeUTF("Sent");
+                Send.writeUTF("Server : " + "Message Received");
+
             } catch (IOException e) {
                 e.printStackTrace();
                 return;
             }
             System.out.println("Query: " + Line);
+        }
+        try
+        {
+            Receive.close();
+            Send.close();
+            socket.close();
+        }
+        catch(IOException i)
+        {
+            System.out.println(i);
         }
 
     }
