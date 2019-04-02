1bea80fdcbd0bcf9a4963d30eee8449e1d3e59c8
adhammamdouh <36306083+adhammamdouh@users.noreply.github.com>
2019-04-02 15:37:49 +0200

    Add files via upload

---
+++ ClientInfo.java
@@ -0,0 +1,6 @@
+import java.net.Socket;
+
+public class ClientInfo {
+    public Socket socket;
+    public String Name;
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        HomePage HP = new HomePage(5000);
+        HP.setVisible(true);
+    }
+}
