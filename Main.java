0f545749981c6d49d673f732670ebbc5f3ac23ff
adhammamdouh <36306083+adhammamdouh@users.noreply.github.com>
2019-04-06 17:17:21 +0200

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        HomePage HP = new HomePage(5000);
-        HP.setVisible(true);
-    }
-}
