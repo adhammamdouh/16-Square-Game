59544387ef9a5f00b721c7eaeedcc2f82e715d48
adhammamdouh <36306083+adhammamdouh@users.noreply.github.com>
2019-04-06 17:17:00 +0200

    Update and rename ClientInfo.java to Hosterinfo.java

--- Server/Hosterinfo.java
+++ Server/Hosterinfo.java
@@ -1,11 +1,11 @@
 import java.net.Socket;
 
-public class ClientInfo {
+public class HosterInfo {
     public Socket socket;
     public String Name;
     public boolean Available;
 
-    public ClientInfo(Socket s,String N,boolean A){
+    public HosterInfo(Socket s, String N, boolean A){
         socket = s;
         Available = A;
         Name = N;
