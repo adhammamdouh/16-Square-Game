9c3d5b5718dc4a5363c03c6d200d1dd322c57feb
adhammamdouh <36306083+adhammamdouh@users.noreply.github.com>
2019-04-06 17:15:56 +0200

    Update MyServer.java

--- Server/MyServer.java
+++ Server/MyServer.java
@@ -1,20 +1,21 @@
 // A Java program for a Server 
-import java.awt.*;
-import java.net.*;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
-public class MyServer
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server
 {
     private Socket MainSocket = null;
     private ServerSocket server = null;
     private DataInputStream _Receive = null;
-    private String Name = null;
 
-    public static volatile ArrayList<ClientInfo> Hoster = null;
+    public static volatile ArrayList<HosterInfo> Hoster = null;
     // constructor with port
-    public MyServer(int port)
+    public Server(int port)
     {
         // starts server and waits for a connection
         try
@@ -37,19 +38,36 @@ public class MyServer
                     _Receive = new DataInputStream(MainSocket.getInputStream());
 
                     Which = _Receive.readUTF();
-                    Name = _Receive.readUTF();
 
                 } catch (IOException e) {
                     System.out.println("I/O error: " + e);
                 }
 
                 if(Which.equals("1")) {
-                    Hoster.add(new ClientInfo(MainSocket,Name,true));
-
+                    Thread HostThread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Socket player = MainSocket;
+                            DataInputStream Receive = null;
+                            DataOutputStream Send = null;
+                            try {
+                                Receive = new DataInputStream(player.getInputStream());
+                                Send = new DataOutputStream(player.getOutputStream());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            String Name = CheckName(Receive,Send);
+                            if(Name.equals("0"))return;
+                            else if(Name.equals("-1"))return;
+                            Hoster.add(new HosterInfo(MainSocket,Name,true));
+                        }
+                    });
+                    HostThread.setDaemon(true);
+                    HostThread.start();
                 }
 
                 else if(Which.equals("0")) {
-                    // new thread for a client
+                    // new thread every two client
                     Thread clientThread = new Thread(new Runnable() {
 
                         Socket Player1 = null;
@@ -59,47 +77,42 @@ public class MyServer
 
                         DataInputStream ReceivePlayer2 = null;
                         DataOutputStream SendPlayer2 = null;
+                        int HostIndex = 0;
 
                         @Override
                         public void run() {
-                            int i = 0;
-                            for(i = 0 ; i < Hoster.size() ; ++i){
-                                if(Hoster.get(i).Available){
-                                    Player1 = Hoster.get(i).socket;
-                                    break;
-                                }
-                            }
-
+                            String Name = null;
                             try {
                                 Player2 = MainSocket;
-                                ReceivePlayer2 = new DataInputStream(Player2.getInputStream());
                                 SendPlayer2 = new DataOutputStream(Player2.getOutputStream());
-                                SendPlayer1 = new DataOutputStream(Player1.getOutputStream());
-                                ReceivePlayer1 = new DataInputStream(Player1.getInputStream());
+                                ReceivePlayer2 = new DataInputStream(Player2.getInputStream());
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
+                            Name = CheckName(ReceivePlayer2,SendPlayer2);
+                            if(Name.equals("0"))return;
+                            if(Name.equals("-1"))return;
+
+                            if(!SendHostersElements())return;
 
                             try {
-
                                 SendPlayer1.writeUTF(Name);
-                                SendPlayer2.writeUTF(Hoster.get(i).Name);
+                                SendPlayer2.writeUTF(Hoster.get(HostIndex).Name);
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
+
 
                             while (true) {
                                 String num = "";
-
                                 num = SendReceive(num,ReceivePlayer1,SendPlayer1,ReceivePlayer2,SendPlayer2);
-
                                 if(num.equals("17"))break;
-                                num = "0";
 
+                                num = "0";
                                 num = SendReceive(num,ReceivePlayer2,SendPlayer2,ReceivePlayer1,SendPlayer1);
                                 if(num.equals("17"))break;
                             }
-                            stop(i);
+                            stop();
                         }
 
                         public String SendReceive(String num, DataInputStream Player1Read,DataOutputStream Player1Send,DataInputStream Player2Read,DataOutputStream Player2Send){
@@ -110,7 +123,8 @@ public class MyServer
                                     try {
                                         Player2Send.writeUTF("error");
                                     } catch (IOException e1) {
-                                        e1.printStackTrace();
+                                        num = "17";
+                                        break;
                                     }
                                     num = "17";
                                     break;
@@ -129,7 +143,8 @@ public class MyServer
                                     try {
                                         Player1Send.writeUTF("error");
                                     } catch (IOException e1) {
-                                        e1.printStackTrace();
+                                        num = "17";
+                                        break;
                                     }
                                     num = "17";
                                     break;
@@ -145,8 +160,97 @@ public class MyServer
                             return num;
                         }
 
-                        public synchronized void stop(int i){
-                            Hoster.remove(i);
+                        public boolean SendHostersElements(){
+                            while (true) {
+                                for (int i = 0; i < Hoster.size(); ++i) {
+                                    try {
+                                        if(Hoster.get(i).Available)
+                                            SendPlayer2.writeUTF(Hoster.get(i).Name);
+                                    } catch (IOException e) {
+                                        return false;
+                                    }
+                                }
+                                try {
+                                    SendPlayer2.writeUTF("end");
+                                } catch (IOException e) {
+                                    return false;
+                                }
+
+                                String HostName = null;
+
+                                try {
+                                    HostName = ReceivePlayer2.readUTF();
+                                } catch (IOException e) {
+                                    return false;
+                                }
+
+                                if (HostName.equals("refresh"))continue;
+                                while (true) {
+                                    if (CheckConnection(HostName)) {
+                                        try {
+                                            SendPlayer1.writeUTF("start");
+                                        } catch (IOException e) {
+                                            for(int i = 0 ; i < Hoster.size() ; ++i){
+                                                if(Hoster.get(i).Name.equals(HostName)){
+                                                    Hoster.remove(i);
+                                                    break;
+                                                }
+                                            }
+                                            return false;
+                                        }
+
+                                        try {
+                                            SendPlayer2.writeUTF("start");
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                        return true;
+                                    } else {
+                                        try {
+                                            SendPlayer2.writeUTF("Not Available");
+                                            HostName = ReceivePlayer2.readUTF();
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                    if(HostName.equals("refresh"))break;
+                                }
+                            }
+                        }
+
+                        public boolean CheckConnection(String Name){
+                            for(int i = 0 ; i < Hoster.size() ; ++i){
+                                if(Hoster.get(i).Name.equals(Name) && Hoster.get(i).Available){
+                                    Player1 = Hoster.get(i).socket;
+                                    try {
+                                        ReceivePlayer1 = new DataInputStream(Player1.getInputStream());
+                                        SendPlayer1 = new DataOutputStream(Player1.getOutputStream());
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                    try {
+                                        SendPlayer1.writeUTF("Connected");
+                                        ReceivePlayer1.readUTF();
+                                    } catch (IOException e) {
+                                        Hoster.remove(i);
+                                        --i;
+                                        return false;
+                                    }
+                                    Hoster.get(i).Available = false;
+                                    HostIndex = i;
+                                    return true;
+                                }
+                            }
+                            return false;
+                        }
+
+                        public synchronized void stop(){
+                            for(int j = 0 ; j < Hoster.size() ; ++j){
+                                if(Hoster.get(j).socket == Player2){
+                                    Hoster.remove(j);
+                                    break;
+                                }
+                            }
                             try {
                                 ReceivePlayer1.close();
                                 SendPlayer1.close();
@@ -164,8 +268,8 @@ public class MyServer
                     clientThread.setDaemon(true);
                     clientThread.start();
                 }
-
             }
+
         }
         catch(IOException i)
         {
@@ -173,8 +277,59 @@ public class MyServer
         }
     }
 
+    public String CheckName(DataInputStream R , DataOutputStream S){
+        String name = null;
+        try {
+            name = R.readUTF();
+        } catch (IOException e) {
+            return "0";
+        }
+        boolean approved = true;
+
+        for(int i = 0 ; i < Hoster.size() ; ++i){
+            Socket socket = Hoster.get(i).socket;
+            if(Hoster.get(i).Name.equals(name)){
+                approved = false;
+                break;
+            }
+            if(!Hoster.get(i).Available)continue;
+
+            DataInputStream Receive = null;
+            DataOutputStream Send = null;
+            try {
+                 Send = new DataOutputStream(socket.getOutputStream());
+                Receive = new DataInputStream(socket.getInputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                Send.writeUTF("connected");
+                Receive.readUTF();
+            } catch (IOException e) {
+                Hoster.remove(i);
+                i--;
+                continue;
+            }
+
+        }
+        if(approved){
+            try {
+                S.writeUTF("1");
+            } catch (IOException e) {
+                return "0";
+            }
+            return name;
+        }
+        try {
+            S.writeUTF("-1");
+        } catch (IOException e) {
+            return "0";
+        }
+        return "-1";
+    }
+
     public static void main(String args[])
     {
-        MyServer server = new MyServer(5001);
+        Server server = new Server(5001);
     }
 } 
